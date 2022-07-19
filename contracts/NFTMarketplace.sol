878b2f8e74f42da559782925f71eabcdc19335ea
obddd <obaidulhaq20@gmail.com>
2022-07-20 00:08:22 +0500

    created basic functions

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,2 +1,85 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFTMarketplace is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    address payable owner;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+    uint256 listPrice = 0.01 ether;
+
+    struct ListedToken {
+        uint256 tokenId;
+        address payable owner;
+        address payable seller;
+        uint256 price;
+        bool currentlyListed;
+    }
+
+    mapping(uint256 => ListedToken) private idToListedToken;
+
+    constructor() ERC721("NFTMarketplace", "NFTM") {
+        owner = payable(msg.sender);
+    }
+
+    //helper function
+    function updateListPrice(uint256 _listPrice) public payable onlyOwner {
+        listPrice = _listPrice;
+    }
+
+    function getListPrice() public view returns (uint256) {
+        return listPrice;
+    }
+
+    function getLatestidToListedToken()
+        public
+        view
+        returns (ListedToken memory)
+    {
+        uint256 _currrentTokenId = _tokenIds.current();
+        return idToListedToken[_currrentTokenId];
+    }
+
+    function getListedForTokenId(uint256 _tokenId)
+        public
+        view
+        returns (ListedToken memory)
+    {
+        return idToListedToken[_tokenId];
+    }
+
+    function getCurrentToken() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIds.current();
+        _tokenIds.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
